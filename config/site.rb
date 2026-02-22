667df66773e1439bd89c5343c08add425f0e1ba6
Javier Julio <jjfutbol@gmail.com>
2026-02-22 01:01:07 -0500

    Clean up Sitepress config

--- config/site.rb
+++ config/site.rb
@@ -1,6 +1,4 @@
-# Default layout for Sitepress pages
 Sitepress.configure do |config|
-  ## Change the root_path of the Sitepress site, or set to a different
-  ## Sitepress instance.
+  # Change the Sitepress root_path or set a different Sitepress instance.
   # config.site = Sitepress::Site.new root_path: "."
 end
